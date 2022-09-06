81faec2231b00953618baa0f9fd84a477efee78b
thx64-tech <thx64.pau@sfr.fr>
2022-09-06 16:17:13 +0200

    maj joueur

--- Joueur.py
+++ Joueur.py
@@ -1,1 +1,15 @@
-#script joueur
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Joueur:
+    def __init__(self):
+        self.nom=""
+        self.sante = 100
+        self.sante_max = 100
+        self.force = 10
+        self.armure = 10
+
+nom = input("Entrez le nom du joueur : ")
+player = Joueur()
+player.nom = nom
+print(f"Le joueur {player.nom} a {player.sante} de vie et {player.force} de force et {player.armure} d'armure")
