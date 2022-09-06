a3f2f8fc691e2a86694d76e5100fc39817e36de0
thx64-tech <thx64.pau@sfr.fr>
2022-09-06 16:26:38 +0200

    maj compteur

--- compteur.py
+++ compteur.py
@@ -1,1 +1,15 @@
-#script compteur
+#!/bin/env#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Joueur():
+    def __init__(self):
+        super().__init__()
+        self.pseudo=""
+        self.health=20
+        self.force=8
+        self.armure=5
+
+joueur=Joueur()
+compteur=0
+while joueur.health != 0:
+    compteur += 1
